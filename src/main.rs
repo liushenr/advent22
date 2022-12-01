80eae4d04d40dd510994df72faae7fe8439919f8
Kei Liu <68406935+liushenr@users.noreply.github.com>
2022-12-01 14:17:11 -0500

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+const SAMPLE: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
+
+fn load_input() -> String {
+    std::fs::read_to_string("input.txt").unwrap()
+}
+
+fn main() {
+    let input = load_input();
+    let mut elves = p1(&input);
+
+    //sort elves value desc
+    elves.sort();
+    elves.reverse();
+
+    //adding the first three
+    dbg!(elves[..3].iter().sum::<usize>());
+    //println!("{}", input);
+}
+
+fn p1(inp: &str) -> Vec<usize> {
+        //a vector to hold elves calories sum
+        let mut elves = Vec::new();
+
+        //hold running total
+        let mut sum = 0;
+
+        for line in inp.lines() {
+            if line.is_empty() {
+                elves.push(sum);
+                sum = 0;
+            } else {
+                sum += line.parse::<usize>().unwrap();
+            }
+        }
+    elves.push(sum);
+    println!("max = {}", elves.iter().max().unwrap());
+    elves
+}
